723e114b699659d993bd91299c24fb1cbc2e5b6b
Sirius <159132908+sirius-22@users.noreply.github.com>
2025-02-28 14:44:03 +0100

    Aggiornamento junit tests

--- Refactoring_Conwaylife25_java/src/test/java/conway/conway25JavaTest.java
+++ Refactoring_Conwaylife25_java/src/test/java/conway/conway25JavaTest.java
@@ -1,7 +1,11 @@
 package conway;
 
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.After;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -22,23 +26,163 @@ public static void setup() {
 	life = new Life( 3,3 );
     cc = new LifeController(life);
 }
+	@Before
+	public void up() {
+		System.out.println("inizio di un test-up");
+		life.resetGrids();
+	}
 
 	@Test
 	public void test1() {
 		System.out.println("inizio test 1");
 		cc.displayGrid();
+		System.out.println("");
 		
+		/*
+			da
+		 * 0 0 0
+		 * 1 1 1
+		 * 0 0 0
+		 * a
+		 * 0 1 0
+		 * 0 1 0
+		 * 0 1 0
+		 */
 		life.switchCellState(1, 0);
 		life.switchCellState(1, 1);
 		life.switchCellState(1, 2);
 		
 		life.computeNextGen();
-		
+		//assert
+		assertFalse(life.isCellAlive(0, 0));
+		assertTrue(life.isCellAlive(0, 1));
+		assertFalse(life.isCellAlive(0, 2));
+		assertFalse(life.isCellAlive(1, 0));
+		assertTrue(life.isCellAlive(1, 1));
+		assertFalse(life.isCellAlive(1, 2));
+		assertFalse(life.isCellAlive(2, 0));
+		assertTrue(life.isCellAlive(2, 1));
+		assertFalse(life.isCellAlive(2, 2));
 		cc.displayGrid();
 	}
+	@Test
+	public void test2() {
+		System.out.println("inizio test 2");
+		cc.displayGrid();
+		System.out.println("");
+		/*
+			da
+		 * 0 1 0
+		 * 0 1 0
+		 * 0 1 0
+		 * a
+		 * 0 0 0
+		 * 1 1 1
+		 * 0 0 0
+		 */
+		life.switchCellState(0, 1);
+		life.switchCellState(1, 1);
+		life.switchCellState(2, 1);
+		
+		life.computeNextGen();
+		//assert
+		assertFalse(life.isCellAlive(0, 0));
+		assertFalse(life.isCellAlive(0, 1));
+		assertFalse(life.isCellAlive(0, 2));
+		assertTrue(life.isCellAlive(1, 0));
+		assertTrue(life.isCellAlive(1, 1));
+		assertTrue(life.isCellAlive(1, 2));
+		assertFalse(life.isCellAlive(2, 0));
+		assertFalse(life.isCellAlive(2, 1));
+		assertFalse(life.isCellAlive(2, 2));
+		cc.displayGrid();
+	}
+	@Test
+	public void test3(){
+		//calcolare fino alla seconda generazione
+		System.out.println("inizio test 3");
+		cc.displayGrid();
+		System.out.println("");
+		/*
+			da
+		 * 0 1 0
+		 * 0 1 0
+		 * 0 1 0
+		 * a
+		 * 0 0 0
+		 * 1 1 1
+		 * 0 0 0
+		 * a
+		 * 0 1 0
+		 * 0 1 0
+		 * 0 1 0
+		 */
+		life.switchCellState(0, 1);
+		life.switchCellState(1, 1);
+		life.switchCellState(2, 1);
+
+		life.computeNextGen();
+		//assert
+		assertFalse(life.isCellAlive(0, 0));
+		assertFalse(life.isCellAlive(0, 1));
+		assertFalse(life.isCellAlive(0, 2));
+		assertTrue(life.isCellAlive(1, 0));
+		assertTrue(life.isCellAlive(1, 1));
+		assertTrue(life.isCellAlive(1, 2));
+		assertFalse(life.isCellAlive(2, 0));
+		assertFalse(life.isCellAlive(2, 1));
+		assertFalse(life.isCellAlive(2, 2));
+		cc.displayGrid();
+		System.out.println("");
+		life.computeNextGen();
+		//assert
+		assertFalse(life.isCellAlive(0, 0));
+		assertTrue(life.isCellAlive(0, 1));
+		assertFalse(life.isCellAlive(0, 2));
+		assertFalse(life.isCellAlive(1, 0));
+		assertTrue(life.isCellAlive(1, 1));
+		assertFalse(life.isCellAlive(1, 2));
+		assertFalse(life.isCellAlive(2, 0));
+		assertTrue(life.isCellAlive(2, 1));
+		assertFalse(life.isCellAlive(2, 2));
+		cc.displayGrid();
+
+	}
+
+	@Test
+	public void oneCellDies(){
+		System.out.println("inizio test oneCellDies");
+		cc.displayGrid();
+		System.out.println("");
+		/*
+			da
+		 * 0 0 0
+		 * 0 1 0
+		 * 0 0 0
+		 * a
+		 * 0 0 0
+		 * 0 0 0
+		 * 0 0 0
+		 */
+		life.switchCellState(1, 1);
+		life.computeNextGen();
+		//assert
+		assertFalse(life.isCellAlive(0, 0));
+		assertFalse(life.isCellAlive(0, 1));
+		assertFalse(life.isCellAlive(0, 2));
+		assertFalse(life.isCellAlive(1, 0));
+		assertFalse(life.isCellAlive(1, 1));
+		assertFalse(life.isCellAlive(1, 2));
+		assertFalse(life.isCellAlive(2, 0));
+		assertFalse(life.isCellAlive(2, 1));
+		assertFalse(life.isCellAlive(2, 2));
+		cc.displayGrid();
+	}
+	@Test
+
 	@After
 	public void down() {
-		System.out.println("down");
+		System.out.println("FINE di un test-down\n");
 	}
 	
 
